7c0cdb6446fd3f27c13c5e191356097044fbf5da
KevinEs14 <escobarkev@gmail.com>
2021-04-18 17:14:18 -0400

    creation of LoginPage and implementation of card component

---
+++ vic19web/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <CardLogin ></CardLogin>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import CardLogin from '@/components/Login/CardLogin.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+    CardLogin,
+  }
+}
+</script>
